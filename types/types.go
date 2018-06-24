4a8ff9db1ecf97772bed3ae9a2848954b8774fb6
Rafael dos Santos <rafaveira3@gmail.com>
2018-06-24 11:31:57 -0300

    Including securityTest struct.

--- types/types.go
+++ types/types.go
@@ -2,7 +2,7 @@ package types
 
 import "gopkg.in/mgo.v2/bson"
 
-// Repository is the struct of all data from repository to be analyzed
+// Repository is the struct of all data from repository to be analyzed.
 type Repository struct {
 	ID           bson.ObjectId `bson:"_id,omitempty"`
 	URL          string        `json:"repositoryURL" bson:"URL"`
@@ -10,4 +10,12 @@ type Repository struct {
 	SecurityTest []string      `bson:"securityTest"`
 	CreatedAt    string        `bson:"createdAt"`
 	DeletedAt    string        `bson:"deletedAt"`
+}
+
+// SecurityTest is the struct of all data from the security tests to be executed.
+type SecurityTest struct {
+	ID    bson.ObjectId `bson:"_id,omitempty"`
+	Name  string        `bson:"name"`
+	Image string        `bson:"image"`
+	Cmd   []string      `bson:"cmd"`
 }
