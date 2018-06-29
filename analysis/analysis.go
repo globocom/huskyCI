87fe99559ad949781aff6f51ba1c3ec6b8852cae
Rafael dos Santos <rafaveira3@gmail.com>
2018-06-29 14:48:42 -0300

    Adding CreateNewRepository and updating ReceiveRequest.

--- analysis/analysis.go
+++ analysis/analysis.go
@@ -1,14 +1,12 @@
 package analysis
 
 import (
-	"fmt"
 	"net/http"
 
 	"gopkg.in/mgo.v2"
 
 	"github.com/globocom/husky/types"
 	"github.com/labstack/echo"
-	"gopkg.in/mgo.v2/bson"
 )
 
 // HealthCheck is the heath check function.
@@ -16,108 +14,48 @@ func HealthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "WORKING!\n")
 }
 
-// StartAnalysis starts the analysis.
-func StartAnalysis(c echo.Context) error {
+// ReceiveRequest receives the request and performs several checks before starting a new analysis.
+func ReceiveRequest(c echo.Context) error {
 	RID := c.Response().Header()["X-Request-Id"][0]
+
+	// check-01: is this a valid JSON?
 	repository := types.Repository{}
 	err := c.Bind(&repository)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"RID": RID, "result": "error", "details": "Error binding repository."})
 	}
 
-	// essa URL é um repositorio git? verificar bibliotecas default.
+	// check-02: is this repository in MongoDB?
+	repositoryQuery := map[string]interface{}{"URL": repository.URL}
+	repositoryResult, err := FindOneDBRepository(repositoryQuery)
+	if err != nil {
+		return c.JSON(http.StatusNotFound, map[string]string{"RID": RID, "result": "error", "details": "Repository not found."})
+	}
 
-	// does this URL have already a running status analysis?
-	// same commit ok! check this
+	// check-03: does this repository have a running status analysis? (for the future: check commits and not URLs?)
 	analysisQuery := map[string]interface{}{"URL": repository.URL}
 	analysisResult, err := FindOneDBAnalysis(analysisQuery)
 	if err != mgo.ErrNotFound {
-		// found an analysis for this URL. Is it running?
 		if analysisResult.Status == "running" {
 			return c.JSON(http.StatusConflict, map[string]string{"RID": RID, "result": "error", "details": "An analysis is already in place for this URL."})
 		}
 	}
 
-	// does this URL have already a document in MongoDB?
-	repositoryQuery := map[string]interface{}{"URL": repository.URL}
-	repositoryResult, err := FindOneDBRepository(repositoryQuery)
-	if err == mgo.ErrNotFound {
-		// inserting a new document for this URL.
-		repositoryResult, err = InsertDBRepository(repository)
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"RID": RID, "result": "error", "details": "Internal error. InsertDBNewRepository()."})
-		}
-	} else {
-		// what if MongoDB is not reachable!?
-		// case! check this
-		//return c.JSON(http.StatusInternalServerError, map[string]string{"RID": RID, "result": "error", "details": "Internal error. Check MongoDB. FindBDRepository()."})
-	}
-
-	// analysis info to be included later into MongoDB.
-	newAnalysis := types.Analysis{
-		RID:            RID,
-		URL:            repositoryResult.URL,
-		SecurityTestID: repositoryResult.SecurityTestID,
-		Status:         "running",
-		Result:         "",
-	}
-
-	// starting each securityTest for URL
-	for _, securityTestID := range repositoryResult.SecurityTestID {
-		CID, err := StartSecurityTest(RID, securityTestID, repository)
-		if err != nil {
-			// change this! if one fails, try again. maxTries
-			// glbgelf include!!!! https://gitlab.globoi.com/supseg/glbgelf
-			c.Logger().Info("e")
-			return c.JSON(http.StatusInternalServerError, map[string]string{"RID": RID, "result": "error", "details": "Internal error. StartSecurityTest()."})
-		}
-		newAnalysis.CID = append(newAnalysis.CID, CID)
-	}
-
-	_, err = InsertDBAnalysis(newAnalysis)
+	err = StartAnalysis(repositoryResult)
 	if err != nil {
-		fmt.Print("Error", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"RID": RID, "result": "error", "details": "Could not start analysis. Internal error."})
 	}
 
 	return c.JSON(http.StatusOK, map[string]string{"RID": RID, "result": "ok", "details": "Request received."})
 }
 
-// StartSecurityTest starts a given securityTestID in a given repository and returns the containerID.
-func StartSecurityTest(RID string, securityTestID bson.ObjectId, r types.Repository) (string, error) {
-	// securityTestQuery := map[string]interface{}{"_id": securityTestID}
-	// securityTestReponse, err := FindSecurityTest(securityTestQuery)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// docker := dockerapi.Docker{}
-	// containerID, err := docker.CreateContainer(securityTestReponse.Name, securityTestReponse.Cmd)
-	// if err != nil {
-	// 	fmt.Println("Erro!", err)
-	// }
-	// fmt.Println("Sucesso!", containerID)
-
-	newContainer := types.Container{
-		CID:            "123asdvxcv12",
-		RID:            RID,
-		VM:             "10.10.10.10",
-		SecurityTestID: securityTestID,
-		CStatus:        "running",
-		COuput:         []string{},
-	}
-
-	_, err := InsertDBContainer(newContainer)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	return newContainer.CID, nil
+// StartAnalysis starts the analysis given a repository.
+func StartAnalysis(repository types.Repository) error {
+	return nil
 }
 
 // StatusAnalysis returns the status of a given analysis (via RID).
 func StatusAnalysis(c echo.Context) error {
-
-	// checking if given RID is present into AnalysisCollection.
 	RID := c.Param("id")
 	analysisQuery := map[string]interface{}{"RID": RID}
 	analysisResult, err := FindOneDBAnalysis(analysisQuery)
@@ -126,62 +64,49 @@ func StatusAnalysis(c echo.Context) error {
 	} else {
 		// What if DB is not reachable!?
 	}
-
-	// checking all securityTests associated with this analysis into SecurityTestCollection.
-	securityTestList := []types.SecurityTest{}
-	for _, securityID := range analysisResult.SecurityTestID {
-		securityTestQuery := map[string]interface{}{"_id": securityID}
-		securityTestResult, err := FindOneDBSecurityTest(securityTestQuery)
-		if err != nil {
-			fmt.Println("Error", err)
-			return err
-		}
-		securityTestList = append(securityTestList, securityTestResult)
-	}
-
-	// checking all containers associated with this analysis into ContainerCollection
-	containerList := []types.Container{}
-	for _, CID := range analysisResult.CID {
-		containerQuery := map[string]interface{}{"CID": CID}
-		containerResult, err := FindOneDBContainer(containerQuery)
-		if err != nil {
-			fmt.Println("Error", err)
-			return err
-		}
-		containerList = append(containerList, containerResult)
-	}
-
-	statusFound := map[string]interface{}{
-		"RID":          RID,
-		"URL":          analysisResult.URL,
-		"result":       analysisResult.Result,
-		"status":       analysisResult.Status,
-		"securityTest": securityTestList,
-		"container":    containerList,
-	}
-
-	return c.JSON(http.StatusFound, statusFound)
+	return c.JSON(http.StatusFound, analysisResult)
 }
 
 // CreateNewSecurityTest inserts the given securityTest into SecurityTestCollection.
 func CreateNewSecurityTest(c echo.Context) error {
-	RID := c.Response().Header()["X-Request-Id"][0]
 	securityTest := types.SecurityTest{}
 	err := c.Bind(&securityTest)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"RID": RID, "result": "error", "details": "Error binding securityTest."})
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error binding securityTest."})
 	}
 
 	securityTestQuery := map[string]interface{}{"name": securityTest.Name}
 	_, err = FindOneDBSecurityTest(securityTestQuery)
 	if err != mgo.ErrNotFound {
-		return c.JSON(http.StatusConflict, map[string]string{"RID": RID, "result": "error", "details": "This securityTest is already in MongoDB."})
+		return c.JSON(http.StatusConflict, map[string]string{"result": "error", "details": "This securityTest is already in MongoDB."})
 	}
 
 	_, err = InsertDBSecurityTest(securityTest)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]string{"RID": RID, "result": "error", "details": "Error creating new securityTest."})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"result": "error", "details": "Error creating new securityTest."})
 	}
 
-	return c.JSON(http.StatusCreated, map[string]string{"RID": RID, "result": "created", "details": "securityTest sucessfully created."})
+	return c.JSON(http.StatusCreated, map[string]string{"result": "created", "details": "securityTest sucessfully created."})
+}
+
+// CreateNewRepository inserts the given repository into RepositoryCollection.
+func CreateNewRepository(c echo.Context) error {
+	repository := types.Repository{}
+	err := c.Bind(&repository)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"result": "error", "details": "Error binding repository."})
+	}
+
+	repositoryQuery := map[string]interface{}{"URL": repository.URL}
+	_, err = FindOneDBRepository(repositoryQuery)
+	if err == nil {
+		return c.JSON(http.StatusConflict, map[string]string{"result": "error", "details": "Repository found."})
+	}
+
+	_, err = InsertDBRepository(repository)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"result": "error", "details": "Error creating new repository."})
+	}
+
+	return c.JSON(http.StatusCreated, map[string]string{"result": "created", "details": "repository sucessfully created."})
 }
