3c515be3a5c3c100e25189f33a993c56f5c5d402
Rafael dos Santos <rafaveira3@gmail.com>
2018-07-27 14:11:59 -0300

    [FEAT] Add client skeleton

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/globocom/husky-client/analysis"
+	"github.com/globocom/husky-client/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// step 0: set all configs needed.
+	config.SetConfigs()
+
+	// step 1: start analysis using received parameters and get a RID.
+	RID, err := analysis.StartAnalysis()
+	if err != nil {
+		fmt.Println("Error StartAnalysis():", err)
+	}
+
+	// step 2: keep querying husky API to check if a given analysis has already finished.
+	huskyAnalysis, err := analysis.MonitorAnalysis(RID)
+	if err != nil {
+		fmt.Println("Error MonitorAnalysis():", err)
+	}
+
+	// step 3: analyze result and return to CI the final result.
+	finalResultError := analysis.AnalyzeResult(huskyAnalysis)
+	if finalResultError != nil {
+		io.WriteString(os.Stderr, "[HUSKY][FAILED]")
+	} else {
+		io.WriteString(os.Stdout, "[HUSKY][PASSED]")
+	}
 }
---
+++ types/types.go
@@ -0,0 +1,52 @@
+package types
+
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+// JSONPayload is a struct that represents the JSON payload needed to make a Husky API request.
+type JSONPayload struct {
+	RepositoryURL string `json:"repositoryURL"`
+}
+
+// JSONResponse is a struct that represents the JSON reponse of a Husky API request.
+type JSONResponse struct {
+	RID     string `json:"RID"`
+	Details string `json:"details"`
+	Result  string `json:"result"`
+}
+
+// Analysis is the struct that stores all data from analysis performed.
+type Analysis struct {
+	ID            bson.ObjectId  `bson:"_id,omitempty"`
+	RID           string         `bson:"RID" json:"RID"`
+	URL           string         `bson:"URL" json:"URL"`
+	SecurityTests []SecurityTest `bson:"securityTests" json:"securityTests"`
+	Status        string         `bson:"status" json:"status"`
+	Result        string         `bson:"result" json:"result"`
+	Containers    []Container    `bson:"containers" json:"containers"`
+}
+
+// Container is the struct that stores all data from a container run.
+type Container struct {
+	CID          string       `bson:"CID" json:"CID"`
+	VM           string       `bson:"VM" json:"VM"`
+	SecurityTest SecurityTest `bson:"securityTest" json:"securityTest"`
+	CStatus      string       `bson:"cStatus" json:"cStatus"`
+	COuput       string       `bson:"cOutput" json:"cOutput"`
+	CResult      string       `bson:"cResult" json:"cResult"`
+	StartedAt    time.Time    `bson:"startedAt" json:"startedAt"`
+	FinishedAt   time.Time    `bson:"finishedAt" json:"finishedAt"`
+}
+
+// SecurityTest is the struct that stores all data from the security tests to be executed.
+type SecurityTest struct {
+	ID       bson.ObjectId `bson:"_id,omitempty"`
+	Name     string        `bson:"name" json:"name"`
+	Image    string        `bson:"image" json:"image"`
+	Cmd      string        `bson:"cmd" json:"cmd"`
+	Language string        `bson:"language" json:"language"`
+	Default  bool          `bson:"default" json:"default"`
+}
