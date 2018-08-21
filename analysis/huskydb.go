08c999dcf5ab4dde597db33f603ecaf5cb1342ec
Rafael dos Santos <rafaveira3@gmail.com>
2018-08-21 19:03:06 -0300

    [FEAT] Add an error verification to db.Connect()
    Now that Connect() returns an error, a check is needed into all
    functions that calls it.
    
    Closing issue #47

--- analysis/huskydb.go
+++ analysis/huskydb.go
@@ -12,88 +12,110 @@ import (
 
 // FindOneDBRepository checks if a given repository is present into RepositoryCollection.
 func FindOneDBRepository(mapParams map[string]interface{}) (types.Repository, error) {
-	session := db.Connect()
+	repositoryResponse := types.Repository{}
+	session, err := db.Connect()
+	if err != nil {
+		return repositoryResponse, err
+	}
 	repositoryQuery := []bson.M{}
 	for k, v := range mapParams {
 		repositoryQuery = append(repositoryQuery, bson.M{k: v})
 	}
 	repositoryFinalQuery := bson.M{"$and": repositoryQuery}
-	repositoryResponse := types.Repository{}
-	err := session.SearchOne(repositoryFinalQuery, nil, db.RepositoryCollection, &repositoryResponse)
+	err = session.SearchOne(repositoryFinalQuery, nil, db.RepositoryCollection, &repositoryResponse)
 	return repositoryResponse, err
 }
 
 // FindOneDBSecurityTest checks if a given securityTest is present into SecurityTestCollection.
 func FindOneDBSecurityTest(mapParams map[string]interface{}) (types.SecurityTest, error) {
-	session := db.Connect()
+	securityTestResponse := types.SecurityTest{}
+	session, err := db.Connect()
+	if err != nil {
+		return securityTestResponse, err
+	}
 	securityTestQuery := []bson.M{}
 	for k, v := range mapParams {
 		securityTestQuery = append(securityTestQuery, bson.M{k: v})
 	}
 	securityTestFinalQuery := bson.M{"$and": securityTestQuery}
-	securityTestResponse := types.SecurityTest{}
-	err := session.SearchOne(securityTestFinalQuery, nil, db.SecurityTestCollection, &securityTestResponse)
+	err = session.SearchOne(securityTestFinalQuery, nil, db.SecurityTestCollection, &securityTestResponse)
 	return securityTestResponse, err
 }
 
 // FindOneDBAnalysis checks if a given analysis is present into AnalysisCollection.
 func FindOneDBAnalysis(mapParams map[string]interface{}) (types.Analysis, error) {
-	session := db.Connect()
+	analysisResponse := types.Analysis{}
+	session, err := db.Connect()
+	if err != nil {
+		return analysisResponse, err
+	}
 	analysisQuery := []bson.M{}
 	for k, v := range mapParams {
 		analysisQuery = append(analysisQuery, bson.M{k: v})
 	}
 	analysisFinalQuery := bson.M{"$and": analysisQuery}
-	analysisResponse := types.Analysis{}
-	err := session.SearchOne(analysisFinalQuery, nil, db.AnalysisCollection, &analysisResponse)
+
+	err = session.SearchOne(analysisFinalQuery, nil, db.AnalysisCollection, &analysisResponse)
 	return analysisResponse, err
 }
 
 // FindAllDBRepository returns all Repository of a given query present into RepositoryCollection.
 func FindAllDBRepository(mapParams map[string]interface{}) ([]types.Repository, error) {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return nil, err
+	}
 	repositoryQuery := []bson.M{}
 	for k, v := range mapParams {
 		repositoryQuery = append(repositoryQuery, bson.M{k: v})
 	}
 	repositoryFinalQuery := bson.M{"$and": repositoryQuery}
 	repositoryResponse := []types.Repository{}
-	err := session.Search(repositoryFinalQuery, nil, db.RepositoryCollection, &repositoryResponse)
+	err = session.Search(repositoryFinalQuery, nil, db.RepositoryCollection, &repositoryResponse)
 	return repositoryResponse, err
 }
 
 // FindAllDBSecurityTest returns all SecurityTests of a given query present into SecurityTestCollection.
 func FindAllDBSecurityTest(mapParams map[string]interface{}) ([]types.SecurityTest, error) {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return nil, err
+	}
 	securityTestQuery := []bson.M{}
 	for k, v := range mapParams {
 		securityTestQuery = append(securityTestQuery, bson.M{k: v})
 	}
 	securityTestFinalQuery := bson.M{"$and": securityTestQuery}
 	securityTestResponse := []types.SecurityTest{}
-	err := session.Search(securityTestFinalQuery, nil, db.SecurityTestCollection, &securityTestResponse)
+	err = session.Search(securityTestFinalQuery, nil, db.SecurityTestCollection, &securityTestResponse)
 	return securityTestResponse, err
 }
 
 // FindAllDBAnalysis returns all Analysis of a given query present into AnalysisCollection.
 func FindAllDBAnalysis(mapParams map[string]interface{}) ([]types.Analysis, error) {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return nil, err
+	}
 	analysisQuery := []bson.M{}
 	for k, v := range mapParams {
 		analysisQuery = append(analysisQuery, bson.M{k: v})
 	}
 	analysisFinalQuery := bson.M{"$and": analysisQuery}
 	analysisResponse := []types.Analysis{}
-	err := session.Search(analysisFinalQuery, nil, db.AnalysisCollection, &analysisResponse)
+	err = session.Search(analysisFinalQuery, nil, db.AnalysisCollection, &analysisResponse)
 	return analysisResponse, err
 }
 
 // InsertDBRepository inserts a new repository with default securityTests into RepositoryCollection.
 func InsertDBRepository(repository types.Repository) error {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return err
+	}
 	repository.CreatedAt = time.Now()
 	securityTestList := []types.SecurityTest{}
-	err := errors.New("")
+	err = errors.New("")
 	maxQueryAllowed := 8
 
 	if len(repository.SecurityTestName) == 0 {
@@ -144,7 +166,10 @@ func InsertDBRepository(repository types.Repository) error {
 
 // InsertDBSecurityTest inserts a new securityTest into SecurityTestCollection.
 func InsertDBSecurityTest(securityTest types.SecurityTest) error {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return err
+	}
 	newSecurityTest := bson.M{
 		"name":     securityTest.Name,
 		"image":    securityTest.Image,
@@ -152,13 +177,16 @@ func InsertDBSecurityTest(securityTest types.SecurityTest) error {
 		"language": securityTest.Language,
 		"default":  securityTest.Default,
 	}
-	err := session.Insert(newSecurityTest, db.SecurityTestCollection)
+	err = session.Insert(newSecurityTest, db.SecurityTestCollection)
 	return err
 }
 
 // InsertDBAnalysis inserts a new analysis into AnalysisCollection.
 func InsertDBAnalysis(analysis types.Analysis) error {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return err
+	}
 	newAnalysis := bson.M{
 		"RID":          analysis.RID,
 		"URL":          analysis.URL,
@@ -167,55 +195,67 @@ func InsertDBAnalysis(analysis types.Analysis) error {
 		"result":       analysis.Result,
 		"containers":   analysis.Containers,
 	}
-	err := session.Insert(newAnalysis, db.AnalysisCollection)
+	err = session.Insert(newAnalysis, db.AnalysisCollection)
 	return err
 }
 
 // UpdateOneDBRepository checks if a given repository is present into RepositoryCollection and update it.
 func UpdateOneDBRepository(mapParams, updateQuery map[string]interface{}) error {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return err
+	}
 	repositoryQuery := []bson.M{}
 	for k, v := range mapParams {
 		repositoryQuery = append(repositoryQuery, bson.M{k: v})
 	}
 	repositoryFinalQuery := bson.M{"$and": repositoryQuery}
-	err := session.Update(repositoryFinalQuery, updateQuery, db.RepositoryCollection)
+	err = session.Update(repositoryFinalQuery, updateQuery, db.RepositoryCollection)
 	return err
 }
 
 // UpdateOneDBSecurityTest checks if a given securityTest is present into SecurityTestCollection and update it.
 func UpdateOneDBSecurityTest(mapParams map[string]interface{}, updatedSecurityTest types.SecurityTest) error {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return err
+	}
 	securityTestQuery := []bson.M{}
 	for k, v := range mapParams {
 		securityTestQuery = append(securityTestQuery, bson.M{k: v})
 	}
 	securityTestFinalQuery := bson.M{"$and": securityTestQuery}
-	err := session.Update(securityTestFinalQuery, updatedSecurityTest, db.SecurityTestCollection)
+	err = session.Update(securityTestFinalQuery, updatedSecurityTest, db.SecurityTestCollection)
 	return err
 }
 
 // UpdateOneDBAnalysis checks if a given analysis is present into AnalysisCollection and update it.
 func UpdateOneDBAnalysis(mapParams map[string]interface{}, updatedAnalysis types.Analysis) error {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return err
+	}
 	analysisQuery := []bson.M{}
 	for k, v := range mapParams {
 		analysisQuery = append(analysisQuery, bson.M{k: v})
 	}
 	analysisFinalQuery := bson.M{"$and": analysisQuery}
-	err := session.Update(analysisFinalQuery, updatedAnalysis, db.AnalysisCollection)
+	err = session.Update(analysisFinalQuery, updatedAnalysis, db.AnalysisCollection)
 	return err
 }
 
 // UpdateOneDBAnalysisContainer checks if a given analysis is present into AnalysisCollection and update the container associated in it.
 func UpdateOneDBAnalysisContainer(mapParams, updateQuery map[string]interface{}) error {
-	session := db.Connect()
+	session, err := db.Connect()
+	if err != nil {
+		return err
+	}
 	analysisQuery := []bson.M{}
 	for k, v := range mapParams {
 		analysisQuery = append(analysisQuery, bson.M{k: v})
 	}
 	analysisFinalQuery := bson.M{"$and": analysisQuery}
-	err := session.Update(analysisFinalQuery, updateQuery, db.AnalysisCollection)
+	err = session.Update(analysisFinalQuery, updateQuery, db.AnalysisCollection)
 	return err
 }
 
