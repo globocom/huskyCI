c0eef9a28a68477521282419090e097fed9f7505
Rafael dos Santos <rafaveira3@gmail.com>
2018-08-27 11:25:06 -0300

    [FEAT] Add getEnryConfig and getGasConfig
    When Husky started there was no verification to check if default
    security tests (enry and gas) were present into MongoDB.
    
    The user had to read README.md and manually insert default security
    tests via POST("/securitytest").
    
    It was added a config.yaml that will hold all configuration needed to
    add automatically all these configuration into DB when Husky starts.
    
    Viper is being used to get these info from config.yaml.
    
    Closing issue #43

--- context/config.go
+++ context/config.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/globocom/husky/types"
+	"github.com/spf13/viper"
 )
 
 // MongoConfig represents MongoDB configuration.
@@ -29,10 +32,19 @@ type APIConfig struct {
 	MongoDBConfig     *MongoConfig
 	DockerHostsConfig *DockerHostsConfig
 	HuskyAPIPort      int
+	EnrySecurityTest  *types.SecurityTest
+	GasSecurityTest   *types.SecurityTest
 }
 
 var apiConfig *APIConfig
 var onceConfig sync.Once
+
+func init() {
+	// check if Viper's config.yaml is properly set.
+	if err := loadViper(); err != nil {
+		return
+	}
+}
 
 // GetAPIConfig returns the instance of an APIConfig.
 func GetAPIConfig() *APIConfig {
@@ -41,6 +53,8 @@ func GetAPIConfig() *APIConfig {
 			MongoDBConfig:     getMongoConfig(),
 			DockerHostsConfig: getDockerHostsConfig(),
 			HuskyAPIPort:      getAPIHostPort(),
+			EnrySecurityTest:  getEnryConfig(),
+			GasSecurityTest:   getGasConfig(),
 		}
 	})
 	return apiConfig
@@ -111,4 +125,33 @@ func getMongoTimeout() time.Duration {
 		return time.Duration(60) * time.Second
 	}
 	return time.Duration(mongoTimeout) * time.Second
+}
+
+func getEnryConfig() *types.SecurityTest {
+	return &types.SecurityTest{
+		Name:     viper.GetString("enry.name"),
+		Image:    viper.GetString("enry.image"),
+		Cmd:      viper.GetString("enry.cmd"),
+		Language: viper.GetString("enry.language"),
+		Default:  viper.GetBool("enry.default"),
+	}
+}
+
+func getGasConfig() *types.SecurityTest {
+	return &types.SecurityTest{
+		Name:     viper.GetString("gas.name"),
+		Image:    viper.GetString("gas.image"),
+		Cmd:      viper.GetString("gas.cmd"),
+		Language: viper.GetString("gas.language"),
+		Default:  viper.GetBool("gas.default"),
+	}
+}
+
+func loadViper() error {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+	return nil
 }
