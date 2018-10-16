62d3e0712c004c6069886aedbeecc12281b9f27a
Rafael dos Santos <rafaveira3@gmail.com>
2018-10-16 15:18:33 -0300

    [FEAT] Add a validation to env vars

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "os"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 // RepositoryURL stores the repository URL of the project to be analyzed.
 var RepositoryURL string
@@ -8,8 +12,41 @@ var RepositoryURL string
 // HuskyAPI stores the address of Husky's API.
 var HuskyAPI string
 
+// RepositoryBranch stores the repository branch of the project to be analyzed.
+var RepositoryBranch string
+
 // SetConfigs sets all configuration needed to start the client.
 func SetConfigs() {
-	RepositoryURL = os.Getenv(`CI_REPOSITORY_URL`)
-	HuskyAPI = os.Getenv(`HUSKY_API`)
+	RepositoryURL = os.Getenv(`HUSKYCI_REPO_URL`)
+	RepositoryBranch = os.Getenv(`HUSKYCI_REPO_BRANCH`)
+	HuskyAPI = os.Getenv(`HUSKYCI_API`)
+}
+
+// CheckEnvVars checks if all environment vars are set.
+func CheckEnvVars() error {
+
+	envVars := []string{
+		"HUSKYCI_REPO_URL",
+		"HUSKYCI_REPO_BRANCH",
+		"HUSKYCI_API",
+	}
+
+	var envIsSet bool
+	var allEnvIsSet bool
+	var errorString string
+
+	env := make(map[string]string)
+	allEnvIsSet = true
+	for i := 0; i < len(envVars); i++ {
+		env[envVars[i]], envIsSet = os.LookupEnv(envVars[i])
+		if !envIsSet {
+			errorString = errorString + envVars[i] + " "
+			allEnvIsSet = false
+		}
+	}
+	if allEnvIsSet == false {
+		finalError := fmt.Sprintf("%s", errorString)
+		return errors.New(finalError)
+	}
+	return nil
 }
