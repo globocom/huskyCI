f1be35bc7c4af903e002ba2fe1b5cf61abafeeb3
Rafael dos Santos <rafaveira3@gmail.com>
2018-11-12 22:01:54 -0200

    [FIX] Add correct path to run-create-certs

--- dev/run-create-certs.sh
+++ dev/run-create-certs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf dockers/development/certs/*
-./dockers/development/create-certs.sh -m ca -pw yourSecretPassword -t dockers/development/certs -e 900
-./dockers/development/create-certs.sh -m server -h dockerapi -pw yourSecretPassword -t dockers/development/certs -e 365
-./dockers/development/create-certs.sh -m client -h huskyapi -pw yourSecretPassword -t dockers/development/certs -e 365
+rm -rf dev/certs/*
+./dev/create-certs.sh -m ca -pw yourSecretPassword -t dev/certs -e 900
+./dev/create-certs.sh -m server -h dockerapi -pw yourSecretPassword -t dev/certs -e 365
+./dev/create-certs.sh -m client -h huskyapi -pw yourSecretPassword -t dev/certs -e 365
