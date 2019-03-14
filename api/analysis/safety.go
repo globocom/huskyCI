9c3fd5bb938aeb4193e5566e973f25766f1bac37
Daniel Carlier <daniel.carlier@hotmail.com>
2019-03-14 18:31:25 -0300

    [FIX] Fix Safety run in API

--- api/analysis/safety.go
+++ api/analysis/safety.go
@@ -5,6 +5,7 @@
 package analysis
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"strings"
@@ -15,16 +16,16 @@ import (
 
 //SafetyOutput is the struct that holds issues, messages and errors found on a Safety scan.
 type SafetyOutput struct {
-	SafetyIssues []SafetyIssue
+	SafetyIssues []SafetyIssue `json:"issues"`
 }
 
 //SafetyIssue is a struct that holds the results that were scanned and the file they came from.
 type SafetyIssue struct {
-	Dependency string
-	Below      string
-	Version    string
-	Comment    string
-	ID         int
+	Dependency string `json:"dependency"`
+	Below      string `json:"vulnerable_below"`
+	Version    string `json:"installed_version"`
+	Comment    string `json:"description"`
+	ID         string `json:"id"`
 }
 
 //SafetyStartAnalysis analyses the output from Safety and sets cResult based on it.
@@ -32,6 +33,12 @@ func SafetyStartAnalysis(CID string, cOutput string) {
 
 	var cResult string
 	analysisQuery := map[string]interface{}{"containers.CID": CID}
+
+	warningFound := strings.Contains(cOutput, "Warning: unpinned requirement ")
+	if warningFound {
+		tmpcOutput := StringToLastLine(cOutput)
+		cOutput = tmpcOutput
+	}
 
 	// step 0.1: error cloning repository!
 	if strings.Contains(cOutput, "ERROR_CLONING") {
@@ -58,7 +65,7 @@ func SafetyStartAnalysis(CID string, cOutput string) {
 	}
 
 	// step 1.1: Sets the container output to "No issues found" if SafetyIssues returns an empty slice
-	if len(safetyOutput.SafetyIssues) == 0 {
+	if len(safetyOutput.SafetyIssues) == 0 && !warningFound {
 		updateContainerAnalysisQuery := bson.M{
 			"$set": bson.M{
 				"containers.$.cOutput": "No issues found.",
@@ -88,4 +95,25 @@ func SafetyStartAnalysis(CID string, cOutput string) {
 		log.Error("SafetyStartAnalysis", "SAFETY", 2007, err)
 		return
 	}
+}
+
+// StringToLastLine receives a string with multiple lines and returns it's last
+func StringToLastLine(s string) string {
+	var lines []string
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines[len(lines)-1]
+}
+
+// GetAllLinesButLast receives a string with multiple lines and returns all but the last line.
+func GetAllLinesButLast(s string) []string {
+	var lines []string
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	lines = lines[:len(lines)-1]
+	return lines
 }
