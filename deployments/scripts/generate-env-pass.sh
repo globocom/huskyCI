b1d95314c89b1dd41e7604cc8fac95296f1c171a
Rafael dos Santos <rafaveira3@gmail.com>
2019-03-14 14:38:02 -0300

    [FEAT] Add client env vars into .env via script

--- deployments/scripts/generate-env-pass.sh
+++ deployments/scripts/generate-env-pass.sh
@@ -3,6 +3,11 @@
 # This script will generate environment passwords to build the environment.
 #
 
+# huskyCI client default vars
+HUSKYCI_REPO_URL="https://github.com/globocom/huskyci.git"
+HUSKYCI_REPO_BRANCH="master"
+HUSKYCI_API="http://localhost:8888"
+
 # Generating "random" passwords
 CERT_PASSPHRASE_TMP="certPass$RANDOM$RANDOM"
 MONGO_DATABASE_USERNAME_TMP="huskyUser$RANDOM$RANDOM"
@@ -16,6 +21,11 @@ echo "MONGO_DATABASE_PASSWORD=$MONGO_DATABASE_PASSWORD_TMP" >> deployments/docke
 echo "export CERT_PASSPHRASE=\"$CERT_PASSPHRASE_TMP\"" > .env
 echo "export MONGO_DATABASE_USERNAME=\"$MONGO_DATABASE_USERNAME_TMP\"" >> .env
 echo "export MONGO_DATABASE_PASSWORD=\"$MONGO_DATABASE_PASSWORD_TMP\"" >> .env
+
+# Adding default envs vars to run client
+echo "export HUSKYCI_REPO_URL=\"$HUSKYCI_REPO_URL\"" >> .env
+echo "export HUSKYCI_REPO_URL=\"$HUSKYCI_REPO_BRANCH\"" >> .env
+echo "export HUSKYCI_REPO_URL=\"$HUSKYCI_API\"" >> .env
 
 # Preparing script to create mongoDB default user
 cat << EOF > deployments/mongo-init.js
