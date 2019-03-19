adba38e7fe8dd073020c1dbfcb30b8f2341a51bb
Daniel Carlier <daniel.carlier@hotmail.com>
2019-03-19 16:04:29 -0300

    [FEAT] Add HTTPS only functionality to huskyCI client

--- deployments/scripts/run-create-certs.sh
+++ deployments/scripts/run-create-certs.sh
@@ -8,4 +8,4 @@ rm -rf deployments/certs/*
 ./deployments/scripts/create-certs.sh -m ca -pw $CERT_PASSPHRASE -t deployments/certs -e 900
 ./deployments/scripts/create-certs.sh -m server -h dockerapi -pw $CERT_PASSPHRASE -t deployments/certs -e 365
 ./deployments/scripts/create-certs.sh -m client -h huskyapi -pw $CERT_PASSPHRASE -t deployments/certs -e 365
-./deployments/scripts/create-certs.sh -m tls -h dockerapi -pw $CERT_PASSPHRASE -t deployments/certs -e 365
+./deployments/scripts/create-certs.sh -m tls -h dockerapi -pw $CERT_PASSPHRASE -t api -e 365
