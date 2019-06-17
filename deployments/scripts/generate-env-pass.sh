b50fe5fe62f0368c5bd7b2c874475aba37c94385
Rafael dos Santos <rafaveira3@gmail.com>
2019-06-17 11:25:46 -0300

    deployments|FIX: Remove logic to generate a random CERT_PASSPHRASE

--- deployments/scripts/generate-env-pass.sh
+++ deployments/scripts/generate-env-pass.sh
@@ -9,14 +9,8 @@ HUSKYCI_CLIENT_REPO_BRANCH="master"
 HUSKYCI_CLIENT_API_ADDR="http://localhost:8888"
 HUSKYCI_CLIENT_API_USE_HTTPS="false"
 
-# Generating "random" password for certificates
-CERT_PASSPHRASE_TMP="certPass$RANDOM$RANDOM"
-
-# Writing password into .env to be used by run_create_cert.sh
-echo "export CERT_PASSPHRASE=\"$CERT_PASSPHRASE_TMP\"" > .env
-
 # Adding default envs vars to run be used by make run-client
-echo "export HUSKYCI_CLIENT_REPO_URL=\"$HUSKYCI_CLIENT_REPO_URL\"" >> .env
+echo "export HUSKYCI_CLIENT_REPO_URL=\"$HUSKYCI_CLIENT_REPO_URL\"" > .env
 echo "export HUSKYCI_CLIENT_REPO_BRANCH=\"$HUSKYCI_CLIENT_REPO_BRANCH\"" >> .env
 echo "export HUSKYCI_CLIENT_API_ADDR=\"$HUSKYCI_CLIENT_API_ADDR\"" >> .env
 echo "export HUSKYCI_CLIENT_API_USE_HTTPS=\"$HUSKYCI_CLIENT_API_USE_HTTPS\"" >> .env
