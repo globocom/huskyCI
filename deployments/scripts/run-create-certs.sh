d6c8c522a8027c8e621e36512957bcb5b94bd11e
Rafael dos Santos <rafaveira3@gmail.com>
2019-06-17 11:25:46 -0300

    deployments|FIX: Remove logic to generate a random CERT_PASSPHRASE

--- deployments/scripts/run-create-certs.sh
+++ deployments/scripts/run-create-certs.sh
@@ -3,9 +3,8 @@
 # This script will create certs to build the enviroment via Makefile.
 #
 
-. .env
 rm -rf deployments/certs/*
-./deployments/scripts/create-certs.sh -m ca -pw $CERT_PASSPHRASE -t deployments/certs -e 900
-./deployments/scripts/create-certs.sh -m server -h dockerapi -pw $CERT_PASSPHRASE -t deployments/certs -e 365
-./deployments/scripts/create-certs.sh -m client -h huskyapi -pw $CERT_PASSPHRASE -t deployments/certs -e 365
-./deployments/scripts/create-certs.sh -m tls -h dockerapi -pw $CERT_PASSPHRASE -t api -e 365
+./deployments/scripts/create-certs.sh -m ca -pw "huskyCICert" -t deployments/certs -e 900
+./deployments/scripts/create-certs.sh -m server -h dockerapi -pw "huskyCICert" -t deployments/certs -e 365
+./deployments/scripts/create-certs.sh -m client -h huskyapi -pw "huskyCICert" -t deployments/certs -e 365
+./deployments/scripts/create-certs.sh -m tls -h dockerapi -pw "huskyCICert" -t api -e 365
