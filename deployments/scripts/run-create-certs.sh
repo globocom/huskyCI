6e2e37810d3c0ca3fa11329b0b8871d553398c83
Daniel Carlier <daniel.carlier@hotmail.com>
2019-06-19 15:35:07 -0300

    deployments|REFACT: Add cert passphrase directly to run-create-certs script

--- deployments/scripts/run-create-certs.sh
+++ deployments/scripts/run-create-certs.sh
@@ -3,9 +3,8 @@
 # This script will create certs to build the enviroment via Makefile.
 #
 
-. .env
 rm -rf deployments/certs/*
-./deployments/scripts/create-certs.sh -m ca -pw $CERT_PASSPHRASE -t deployments/certs -e 900
-./deployments/scripts/create-certs.sh -m server -h dockerapi -pw $CERT_PASSPHRASE -t deployments/certs -e 365
-./deployments/scripts/create-certs.sh -m client -h huskyapi -pw $CERT_PASSPHRASE -t deployments/certs -e 365
-./deployments/scripts/create-certs.sh -m tls -h dockerapi -pw $CERT_PASSPHRASE -t api -e 365
+./deployments/scripts/create-certs.sh -m ca -pw "huskyCICertPassphrase" -t deployments/certs -e 900
+./deployments/scripts/create-certs.sh -m server -h dockerapi -pw "huskyCICertPassphrase" -t deployments/certs -e 365
+./deployments/scripts/create-certs.sh -m client -h huskyapi -pw "huskyCICertPassphrase" -t deployments/certs -e 365
+./deployments/scripts/create-certs.sh -m tls -h dockerapi -pw "huskyCICertPassphrase" -t api -e 365
