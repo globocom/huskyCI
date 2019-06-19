8e819491150391f198d6a9a0167b5fc2970f5bd7
Rafael dos Santos <rafaveira3@gmail.com>
2019-06-19 18:05:35 -0300

    api|TEST: Add HandlePrivateSSHKey tests

--- api/util/util_test.go
+++ api/util/util_test.go
@@ -1,6 +1,8 @@
 package util_test
 
 import (
+	"os"
+
 	"github.com/globocom/huskyCI/api/util"
 
 	. "github.com/onsi/ginkgo"
@@ -15,7 +17,7 @@ var _ = Describe("Util", func() {
 		inputImage := "secdevLabs/bandit"
 		expected := "globocom_secDevLabs_myBranch_bandit"
 
-		Context("When inputURL, imputBranch and inputImage is not empty", func() {
+		Context("When inputURL, imputBranch and inputImage are not empty", func() {
 			It("Should return a container name based on these params", func() {
 				Expect(util.CreateContainerName(inputURL, inputBranch, inputImage)).To(Equal(expected))
 			})
@@ -43,7 +45,7 @@ var _ = Describe("Util", func() {
 		inputCMD := "git clone -b %GIT_BRANCH% --single-branch %GIT_REPO% code --quiet 2> /tmp/errorGitCloneRetirejs"
 		expected := "git clone -b myBranch --single-branch https://github.com/globocom/secDevLabs.git code --quiet 2> /tmp/errorGitCloneRetirejs"
 
-		Context("When inputRepositoryURL, inputRepositoryBranch and inputCMD is not empty", func() {
+		Context("When inputRepositoryURL, inputRepositoryBranch and inputCMD are not empty", func() {
 			It("Should return a string based on these params", func() {
 				Expect(util.HandleCmd(inputRepositoryURL, inputRepositoryBranch, inputCMD)).To(Equal(expected))
 			})
@@ -61,6 +63,36 @@ var _ = Describe("Util", func() {
 		Context("When inputCMD is empty", func() {
 			It("Should return an empty string.", func() {
 				Expect(util.CreateContainerName(inputRepositoryURL, inputRepositoryBranch, "")).To(Equal(""))
+			})
+		})
+	})
+
+	Describe("HandlePrivateSSHKey", func() {
+
+		rawString := "echo 'GIT_PRIVATE_SSH_KEY' > ~/.ssh/huskyci_id_rsa &&"
+		expectedNotEmpty := "echo 'PRIVKEYTEST' > ~/.ssh/huskyci_id_rsa &&"
+		expectedEmpty := "echo '' > ~/.ssh/huskyci_id_rsa &&"
+
+		Context("When rawString and HUSKYCI_API_GIT_PRIVATE_SSH_KEY are not empty", func() {
+			It("Should return a string based on these params", func() {
+				os.Setenv("HUSKYCI_API_GIT_PRIVATE_SSH_KEY", "PRIVKEYTEST")
+				Expect(util.HandlePrivateSSHKey(rawString)).To(Equal(expectedNotEmpty))
+			})
+		})
+		Context("When rawString is empty and HUSKYCI_API_GIT_PRIVATE_SSH_KEY is not empty", func() {
+			It("Should return an empty string.", func() {
+				Expect(util.HandlePrivateSSHKey("")).To(Equal(""))
+			})
+		})
+		Context("When rawString is not empty and HUSKYCI_API_GIT_PRIVATE_SSH_KEY is empty", func() {
+			It("Should return a string based on these params.", func() {
+				os.Unsetenv("HUSKYCI_API_GIT_PRIVATE_SSH_KEY")
+				Expect(util.HandlePrivateSSHKey(rawString)).To(Equal(expectedEmpty))
+			})
+		})
+		Context("When rawString and HUSKYCI_API_GIT_PRIVATE_SSH_KEY are empty", func() {
+			It("Should return an empty string.", func() {
+				Expect(util.HandlePrivateSSHKey("")).To(Equal(""))
 			})
 		})
 	})
