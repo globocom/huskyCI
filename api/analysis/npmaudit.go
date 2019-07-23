e85361597df4bae6871b97b1b6a444b181d05f99
Rafael dos Santos <rafaveira3@gmail.com>
2019-07-23 11:13:27 -0300

    api|REFACT: Add cleaner code into npmAudit file

--- api/analysis/npmaudit.go
+++ api/analysis/npmaudit.go
@@ -8,16 +8,15 @@ import (
 	"encoding/json"
 	"strings"
 
-	"github.com/globocom/huskyCI/api/db"
 	"github.com/globocom/huskyCI/api/log"
 	"github.com/globocom/huskyCI/api/types"
-	"gopkg.in/mgo.v2/bson"
 )
 
 // NpmAuditOutput is the struct that stores all npm audit output
 type NpmAuditOutput struct {
-	Advisories map[string]Vulnerability `json:"advisories"`
-	Metadata   Metadata                 `json:"metadata"`
+	Advisories    map[string]Vulnerability `json:"advisories"`
+	Metadata      Metadata                 `json:"metadata"`
+	FailedRunning bool
 }
 
 // Vulnerability is the granular output of a security info found
@@ -52,60 +51,32 @@ type VulnerabilitiesSummary struct {
 // NpmAuditStartAnalysis analyses the output from Npm Audit and sets a cResult based on it.
 func NpmAuditStartAnalysis(CID string, cOutput string, RID string) {
 
-	var cResult string
-	analysisQuery := map[string]interface{}{"containers.CID": CID}
-
-	// step 0.1: error cloning repository!
-	if strings.Contains(cOutput, "ERROR_CLONING") {
-		updateContainerAnalysisQuery := bson.M{
-			"$set": bson.M{
-				"containers.$.cResult": "error",
-				"containers.$.cInfo":   "Error clonning repository.",
-			},
-		}
-		err := db.UpdateOneDBAnalysisContainer(analysisQuery, updateContainerAnalysisQuery)
-		if err != nil {
-			log.Error("NpmAuditStartAnalysis", "NPMAUDIT", 2007, "Step 0.1 ", err)
-		}
-		return
-	}
-
-	// step 0.2: repository doesn't have package.json
+	errorClonning := strings.Contains(cOutput, "ERROR_CLONING")
 	failedRunning := strings.Contains(cOutput, "ERROR_RUNNING_NPMAUDIT")
+
+	// step 1: check for any errors when clonning repo
+	if errorClonning {
+		if err := updateInfoAndResultBasedOnCID("Error clonning repository", "error", CID); err != nil {
+			return
+		}
+	}
+
+	// step 2: check for any errors when running securityTest
 	if failedRunning {
-		updateContainerAnalysisQuery := bson.M{
-			"$set": bson.M{
-				"containers.$.cResult": "error",
-				"containers.$.cInfo":   "Internal error running NPM Audit.",
-			},
-		}
-		err := db.UpdateOneDBAnalysisContainer(analysisQuery, updateContainerAnalysisQuery)
-		if err != nil {
-			log.Error("NpmAuditStartAnalysis", "NPMAUDIT", 2007, "Step 0.2 ", err)
+		if err := updateInfoAndResultBasedOnCID("Internal error running NPM Audit.", "error", CID); err != nil {
 			return
 		}
 
-		// get updated analysis based on its RID
-		analysisQuery = map[string]interface{}{"RID": RID}
-		analysis, err := db.FindOneDBAnalysis(analysisQuery)
-		if err != nil {
-			log.Error("NpmAuditStartAnalysis", "NPMAUDIT", 2008, CID, err)
-			return
-		}
-
-		// finally, update analysis with huskyCI results
 		npmAuditOutput := NpmAuditOutput{}
-		analysis.HuskyCIResults.JavaScriptResults.HuskyCINpmAuditOutput = prepareHuskyCINpmAuditResults(npmAuditOutput, failedRunning)
-		err = db.UpdateOneDBAnalysis(analysisQuery, analysis)
-		if err != nil {
-			log.Error("NpmAuditStartAnalysis", "NPMAUDIT", 2007, err)
+		npmAuditOutput.FailedRunning = failedRunning
+		if err := updateHuskyCIResultsBasedOnRID(RID, "npmaudit", npmAuditOutput); err != nil {
 			return
 		}
 
 		return
 	}
 
-	// step 1: Unmarshall cOutput into NpmAuditOutput struct.
+	// step 3: Unmarshall cOutput into NpmAuditOutput struct.
 	npmAuditOutput := NpmAuditOutput{}
 	err := json.Unmarshal([]byte(cOutput), &npmAuditOutput)
 	if err != nil {
@@ -113,56 +84,33 @@ func NpmAuditStartAnalysis(CID string, cOutput string, RID string) {
 		return
 	}
 
-	// step 2: find Issues that have severity "moderate" or "high.
-	cResult = "passed"
+	// step 4: find Issues that have severity "moderate" or "high.
+	cResult := "passed"
+	issueMessage := "No issues found."
 	for _, vulnerability := range npmAuditOutput.Advisories {
 		if vulnerability.Severity == "high" || vulnerability.Severity == "moderate" {
 			cResult = "failed"
+			issueMessage = "Issues found."
 			break
 		}
 	}
-
-	// step 3: update analysis' cResult into AnalyisCollection.
-	issueMessage := "No issues found."
-	if cResult == "failed" {
-		issueMessage = "Issues found."
-	}
-	updateContainerAnalysisQuery := bson.M{
-		"$set": bson.M{
-			"containers.$.cResult": cResult,
-			"containers.$.cInfo":   issueMessage,
-		},
-	}
-	err = db.UpdateOneDBAnalysisContainer(analysisQuery, updateContainerAnalysisQuery)
-	if err != nil {
-		log.Error("NpmAuditStartAnalysis", "NPMAUDIT", 2007, "Step 3 ", err)
+	if err := updateInfoAndResultBasedOnCID(issueMessage, cResult, CID); err != nil {
 		return
 	}
 
-	// step 4: get updated analysis based on its RID
-	analysisQuery = map[string]interface{}{"RID": RID}
-	analysis, err := db.FindOneDBAnalysis(analysisQuery)
-	if err != nil {
-		log.Error("NpmAuditStartAnalysis", "NPMAUDIT", 2008, CID, err)
-		return
-	}
-
-	// step 5: finally, update analysis with huskyCI results
-	analysis.HuskyCIResults.JavaScriptResults.HuskyCINpmAuditOutput = prepareHuskyCINpmAuditResults(npmAuditOutput, failedRunning)
-	err = db.UpdateOneDBAnalysis(analysisQuery, analysis)
-	if err != nil {
-		log.Error("NpmAuditStartAnalysis", "NPMAUDIT", 2007, err)
+	// step 6: finally, update analysis with huskyCI results
+	if err := updateHuskyCIResultsBasedOnRID(RID, "npmaudit", npmAuditOutput); err != nil {
 		return
 	}
 
 }
 
 // prepareHuskyCINpmAuditResults will prepare NpmAudit output to be added into JavaScriptResults struct
-func prepareHuskyCINpmAuditResults(npmAuditOutput NpmAuditOutput, failedRunning bool) types.HuskyCINpmAuditOutput {
+func prepareHuskyCINpmAuditResults(npmAuditOutput NpmAuditOutput) types.HuskyCINpmAuditOutput {
 
 	var huskyCInpmAuditResults types.HuskyCINpmAuditOutput
 
-	if failedRunning {
+	if npmAuditOutput.FailedRunning {
 		npmauditVuln := types.HuskyCIVulnerability{}
 		npmauditVuln.Language = "JavaScript"
 		npmauditVuln.SecurityTool = "NpmAudit"
