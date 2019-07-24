bceb2a851e902c30aebf30e45ec420555b6b9521
carlosljr <carlosjunior@id.uff.br>
2019-07-24 11:03:10 -0300

    [REFACT] Turn CheckHuskyRequirements tests to ginkgo framework
    It standardizes husky's test suites to use ginkgo framework.

--- api/util/api/types.go
+++ api/util/api/types.go
@@ -16,3 +16,26 @@ type CheckUtils struct{}
 type HuskyUtils struct {
 	CheckHandler CheckInterface
 }
+
+type FakeCheck struct {
+	EnvVarsError          error
+	DockerHostsError      error
+	MongoDBError          error
+	EachSecurityTestError error
+}
+
+func (fC *FakeCheck) checkEnvVars() error {
+	return fC.EnvVarsError
+}
+
+func (fC *FakeCheck) checkDockerHosts(configAPI *apiContext.APIConfig) error {
+	return fC.DockerHostsError
+}
+
+func (fC *FakeCheck) checkMongoDB() error {
+	return fC.MongoDBError
+}
+
+func (fC *FakeCheck) checkEachSecurityTest(configAPI *apiContext.APIConfig) error {
+	return fC.EachSecurityTestError
+}
