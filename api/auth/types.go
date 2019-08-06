ce91b750d2e0e7f1c48004ced1fab2f63ecdf989
carlosljr <carlosjunior@id.uff.br>
2019-08-06 19:13:53 -0300

    [REFACT] Refact auth modules

--- api/auth/types.go
+++ api/auth/types.go
@@ -14,7 +14,13 @@ type Pbkdf2Generator interface {
 	GetCredsFromDB(username string) (types.User, error)
 	DecodeSaltValue(salt string) ([]byte, error)
 	GenHashValue(value, salt []byte, iter, keyLen int, h hash.Hash) string
+	GenerateSalt() (string, error)
+	GetHashName() string
+	GetIterations() (int, error)
+	GetKeyLength() (int, error)
 }
+
+type Pbkdf2Caller struct{}
 
 type MongoBasic struct {
 	ClientHandler UserCredsHandler
