10e86f247a6d9d24517bf2b639bc5ac9bf81add2
carlosljr <carlosjunior@id.uff.br>
2019-08-16 18:33:36 -0300

    [REFACT] Define interface external calls and a struct for the functions to be tested
    This REFACT is necessary so we can isolate external calls and make unit tests in functions with logic.

--- api/context/context.go
+++ api/context/context.go
@@ -71,45 +71,89 @@ type APIConfig struct {
 	SafetySecurityTest   *types.SecurityTest
 }
 
+type ExternalCalls struct{}
+
+func (eC *ExternalCalls) SetConfigFile(configName, configPath string) error {
+	viper.SetConfigFile(configName)
+	viper.AddConfigPath(configPath)
+	return viper.ReadInConfig()
+}
+
+func (eC *ExternalCalls) GetEnvironmentVariable(envName string) string {
+	return os.Getenv(envName)
+}
+
+func (eC *ExternalCalls) ConvertStrToInt(str string) (int, error) {
+	return strconv.Atoi(str)
+}
+
+func (eC *ExternalCalls) GetTimeDurationInSeconds(duration int) time.Duration {
+	return time.Duration(duration) * time.Second
+}
+
+func (eC *ExternalCalls) GetStringFromConfigFile(value string) string {
+	return viper.GetString(value)
+}
+
+func (eC *ExternalCalls) GetBoolFromConfigFile(value string) bool {
+	return viper.GetBool(value)
+}
+
+func (eC *ExternalCalls) GetIntFromConfigFile(value string) int {
+	return viper.GetInt(value)
+}
+
+type CallerInterface interface {
+	SetConfigFile(configName, configPath string) error
+	GetStringFromConfigFile(value string) string
+	GetBoolFromConfigFile(value string) bool
+	GetIntFromConfigFile(value string) int
+	GetEnvironmentVariable(envName string) string
+	ConvertStrToInt(str string) (int, error)
+	GetTimeDurationInSeconds(duration int) time.Duration
+}
+
+type DefaultConfig struct {
+	Caller CallerInterface
+}
+
 // GetAPIConfig returns the instance of an APIConfig.
-func GetAPIConfig() *APIConfig {
+func (dF DefaultConfig) GetAPIConfig() (*APIConfig, error) {
 
 	// load Viper using api/config.yml
-	viper.SetConfigName("config")
-	viper.AddConfigPath("api/")
-	if err := viper.ReadInConfig(); err != nil {
+	if err := dF.Caller.SetConfigFile("config", "api/"); err != nil {
 		fmt.Println("Error reading Viper config: ", err)
-		os.Exit(1)
+		return nil, err
 	}
 	SetOnceConfig()
 	return APIConfiguration
 }
 
 // SetOnceConfig sets APIConfiguration once
-func SetOnceConfig() {
+func (dF DefaultConfig) SetOnceConfig() {
 	onceConfig.Do(func() {
 		APIConfiguration = &APIConfig{
-			Port:                 getAPIPort(),
-			Version:              GetAPIVersion(),
-			ReleaseDate:          GetAPIReleaseDate(),
-			UseTLS:               getAPIUseTLS(),
-			GitPrivateSSHKey:     getGitPrivateSSHKey(),
-			GraylogConfig:        getGraylogConfig(),
-			MongoDBConfig:        getMongoConfig(),
-			DockerHostsConfig:    getDockerHostsConfig(),
-			EnrySecurityTest:     getSecurityTestConfig("enry"),
-			GosecSecurityTest:    getSecurityTestConfig("gosec"),
-			BanditSecurityTest:   getSecurityTestConfig("bandit"),
-			BrakemanSecurityTest: getSecurityTestConfig("brakeman"),
-			RetirejsSecurityTest: getSecurityTestConfig("retirejs"),
-			NpmAuditSecurityTest: getSecurityTestConfig("npmaudit"),
-			SafetySecurityTest:   getSecurityTestConfig("safety"),
+			Port:                 dF.GetAPIPort(),
+			Version:              dF.GetAPIVersion(),
+			ReleaseDate:          dF.GetAPIReleaseDate(),
+			UseTLS:               dF.GetAPIUseTLS(),
+			GitPrivateSSHKey:     dF.getGitPrivateSSHKey(),
+			GraylogConfig:        dF.getGraylogConfig(),
+			MongoDBConfig:        dF.getMongoConfig(),
+			DockerHostsConfig:    dF.getDockerHostsConfig(),
+			EnrySecurityTest:     dF.getSecurityTestConfig("enry"),
+			GosecSecurityTest:    dF.getSecurityTestConfig("gosec"),
+			BanditSecurityTest:   dF.getSecurityTestConfig("bandit"),
+			BrakemanSecurityTest: dF.getSecurityTestConfig("brakeman"),
+			RetirejsSecurityTest: dF.getSecurityTestConfig("retirejs"),
+			NpmAuditSecurityTest: dF.getSecurityTestConfig("npmaudit"),
+			SafetySecurityTest:   dF.getSecurityTestConfig("safety"),
 		}
 	})
 }
 
-func getAPIPort() int {
-	apiPort, err := strconv.Atoi(os.Getenv("HUSKYCI_API_PORT"))
+func (dF DefaultConfig) GetAPIPort() int {
+	apiPort, err := dF.Caller.ConvertStrToInt(dF.Caller.GetEnvironmentVariable("HUSKYCI_API_PORT"))
 	if err != nil {
 		apiPort = 8888
 	}
@@ -117,91 +161,91 @@ func getAPIPort() int {
 }
 
 // GetAPIVersion returns current API version
-func GetAPIVersion() string {
+func (dF DefaultConfig) GetAPIVersion() string {
 	return "0.6.0"
 }
 
 // GetAPIReleaseDate returns current API release date
-func GetAPIReleaseDate() string {
+func (dF DefaultConfig) GetAPIReleaseDate() string {
 	return "2019-07-18"
 }
 
-func getAPIUseTLS() bool {
-	option := os.Getenv("HUSKYCI_API_ENABLE_HTTPS")
+func (dF DefaultConfig) GetAPIUseTLS() bool {
+	option := dF.Caller.GetEnvironmentVariable("HUSKYCI_API_ENABLE_HTTPS")
 	if option == "true" || option == "1" || option == "TRUE" {
 		return true
 	}
 	return false
 }
 
-func getGitPrivateSSHKey() string {
-	return os.Getenv("HUSKYCI_API_GIT_PRIVATE_SSH_KEY")
+func (dF DefaultConfig) getGitPrivateSSHKey() string {
+	return dF.Caller.GetEnvironmentVariable("HUSKYCI_API_GIT_PRIVATE_SSH_KEY")
 }
 
-func getGraylogConfig() *GraylogConfig {
+func (dF DefaultConfig) getGraylogConfig() *GraylogConfig {
 	return &GraylogConfig{
-		Address:        os.Getenv("HUSKYCI_LOGGING_GRAYLOG_ADDR"),
-		Protocol:       os.Getenv("HUSKYCI_LOGGING_GRAYLOG_PROTO"),
-		AppName:        os.Getenv("HUSKYCI_LOGGING_GRAYLOG_APP_NAME"),
-		Tag:            os.Getenv("HUSKYCI_LOGGING_GRAYLOG_TAG"),
-		DevelopmentEnv: getGraylogIsDev(),
+		Address:        dF.Caller.GetEnvironmentVariable("HUSKYCI_LOGGING_GRAYLOG_ADDR"),
+		Protocol:       dF.Caller.GetEnvironmentVariable("HUSKYCI_LOGGING_GRAYLOG_PROTO"),
+		AppName:        dF.Caller.GetEnvironmentVariable("HUSKYCI_LOGGING_GRAYLOG_APP_NAME"),
+		Tag:            dF.Caller.GetEnvironmentVariable("HUSKYCI_LOGGING_GRAYLOG_TAG"),
+		DevelopmentEnv: GetGraylogIsDev(),
 	}
 }
 
-func getGraylogIsDev() bool {
-	option := os.Getenv("HUSKYCI_LOGGING_GRAYLOG_DEV")
+func (dF DefaultConfig) GetGraylogIsDev() bool {
+	option := dF.Caller.GetEnvironmentVariable("HUSKYCI_LOGGING_GRAYLOG_DEV")
 	if option == "false" || option == "0" || option == "FALSE" {
 		return false
 	}
 	return true
 }
 
-func getMongoConfig() *MongoConfig {
-	mongoHost := os.Getenv("HUSKYCI_DATABASE_MONGO_ADDR")
-	mongoPort := getMongoPort()
+func (dF DefaultConfig) getMongoConfig() *MongoConfig {
+	mongoHost := dF.Caller.GetEnvironmentVariable("HUSKYCI_DATABASE_MONGO_ADDR")
+	mongoPort := GetMongoPort()
 	mongoAddress := fmt.Sprintf("%s:%d", mongoHost, mongoPort)
 	return &MongoConfig{
 		Address:      mongoAddress,
-		DatabaseName: os.Getenv("HUSKYCI_DATABASE_MONGO_DBNAME"),
-		Username:     os.Getenv("HUSKYCI_DATABASE_MONGO_DBUSERNAME"),
-		Password:     os.Getenv("HUSKYCI_DATABASE_MONGO_DBPASSWORD"),
+		DatabaseName: dF.Caller.GetEnvironmentVariable("HUSKYCI_DATABASE_MONGO_DBNAME"),
+		Username:     dF.Caller.GetEnvironmentVariable("HUSKYCI_DATABASE_MONGO_DBUSERNAME"),
+		Password:     dF.Caller.GetEnvironmentVariable("HUSKYCI_DATABASE_MONGO_DBPASSWORD"),
 		Port:         mongoPort,
-		Timeout:      getMongoTimeout(),
-		PoolLimit:    getMongoPoolLimit(),
+		Timeout:      GetMongoTimeout(),
+		PoolLimit:    GetMongoPoolLimit(),
 	}
 }
 
-func getMongoPort() int {
-	mongoPort, err := strconv.Atoi(os.Getenv("HUSKYCI_DATABASE_MONGO_PORT"))
+func (dF DefaultConfig) GetMongoPort() int {
+	mongoPort, err := dF.Caller.ConvertStrToInt(dF.Caller.GetEnvironmentVariable("HUSKYCI_DATABASE_MONGO_PORT"))
 	if err != nil {
 		return 27017
 	}
 	return mongoPort
 }
 
-func getMongoTimeout() time.Duration {
-	mongoTimeout, err := strconv.Atoi(os.Getenv("HUSKYCI_DATABASE_MONGO_TIMEOUT"))
+func (dF DefaultConfig) GetMongoTimeout() time.Duration {
+	mongoTimeout, err := dF.Caller.ConvertStrToInt(dF.Caller.GetEnvironmentVariable("HUSKYCI_DATABASE_MONGO_TIMEOUT"))
 	if err != nil {
-		return time.Duration(60) * time.Second
+		return dF.Caller.GetTimeDurationInSeconds(60)
 	}
-	return time.Duration(mongoTimeout) * time.Second
+	return dF.Caller.GetTimeDurationInSeconds(mongoTimeout)
 }
 
-func getMongoPoolLimit() int {
-	mongoPoolLimit, err := strconv.Atoi(os.Getenv("HUSKYCI_DATABASE_MONGO_POOL_LIMIT"))
+func (dF DefaultConfig) GetMongoPoolLimit() int {
+	mongoPoolLimit, err := dF.Caller.ConvertStrToInt(dF.Caller.GetEnvironmentVariable("HUSKYCI_DATABASE_MONGO_POOL_LIMIT"))
 	if err != nil && mongoPoolLimit <= 0 {
 		return 1000
 	}
 	return mongoPoolLimit
 }
 
-func getDockerHostsConfig() *DockerHostsConfig {
-	dockerAPIPort := getDockerAPIPort()
-	dockerHostsAddressesEnv := os.Getenv("HUSKYCI_DOCKERAPI_ADDR")
+func (dF DefaultConfig) getDockerHostsConfig() *DockerHostsConfig {
+	dockerAPIPort := GetDockerAPIPort()
+	dockerHostsAddressesEnv := dF.Caller.GetEnvironmentVariable("HUSKYCI_DOCKERAPI_ADDR")
 	dockerHostsAddresses := strings.Split(dockerHostsAddressesEnv, " ")
-	dockerHostsCertificate := os.Getenv("HUSKYCI_DOCKERAPI_CERT_FILE")
-	dockerHostsPathCertificates := os.Getenv("HUSKYCI_DOCKERAPI_CERT_PATH")
-	dockerHostsKey := os.Getenv("HUSKYCI_DOCKERAPI_CERT_KEY")
+	dockerHostsCertificate := dF.Caller.GetEnvironmentVariable("HUSKYCI_DOCKERAPI_CERT_FILE")
+	dockerHostsPathCertificates := dF.Caller.GetEnvironmentVariable("HUSKYCI_DOCKERAPI_CERT_PATH")
+	dockerHostsKey := dF.Caller.GetEnvironmentVariable("HUSKYCI_DOCKERAPI_CERT_KEY")
 	return &DockerHostsConfig{
 		Address:              dockerHostsAddresses[0],
 		DockerAPIPort:        dockerAPIPort,
@@ -209,40 +253,40 @@ func getDockerHostsConfig() *DockerHostsConfig {
 		PathCertificate:      dockerHostsPathCertificates,
 		Key:                  dockerHostsKey,
 		Host:                 fmt.Sprintf("%s:%d", dockerHostsAddresses[0], dockerAPIPort),
-		TLSVerify:            getDockerAPITLSVerify(),
-		MaxContainersAllowed: getMaxContainersAllowed(),
+		TLSVerify:            GetDockerAPITLSVerify(),
+		MaxContainersAllowed: GetMaxContainersAllowed(),
 	}
 }
 
-func getDockerAPIPort() int {
-	dockerAPIport, err := strconv.Atoi(os.Getenv("HUSKYCI_DOCKERAPI_PORT"))
+func (dF DefaultConfig) GetDockerAPIPort() int {
+	dockerAPIport, err := dF.Caller.ConvertStrToInt(dF.Caller.GetEnvironmentVariable("HUSKYCI_DOCKERAPI_PORT"))
 	if err != nil {
 		return 2376
 	}
 	return dockerAPIport
 }
 
-func getDockerAPITLSVerify() int {
-	option := os.Getenv("HUSKYCI_DOCKERAPI_TLS_VERIFY")
+func (dF DefaultConfig) GetDockerAPITLSVerify() int {
+	option := dF.Caller.GetEnvironmentVariable("HUSKYCI_DOCKERAPI_TLS_VERIFY")
 	if option == "false" || option == "0" || option == "FALSE" {
 		return 0
 	}
 	return 1
 }
 
-func getSecurityTestConfig(securityTestName string) *types.SecurityTest {
+func (dF DefaultConfig) getSecurityTestConfig(securityTestName string) *types.SecurityTest {
 	return &types.SecurityTest{
-		Name:             viper.GetString(fmt.Sprintf("%s.name", securityTestName)),
-		Image:            viper.GetString(fmt.Sprintf("%s.image", securityTestName)),
-		Cmd:              viper.GetString(fmt.Sprintf("%s.cmd", securityTestName)),
-		Language:         viper.GetString(fmt.Sprintf("%s.language", securityTestName)),
-		Default:          viper.GetBool(fmt.Sprintf("%s.default", securityTestName)),
-		TimeOutInSeconds: viper.GetInt(fmt.Sprintf("%s.timeOutInSeconds", securityTestName)),
+		Name:             dF.Caller.GetStringFromConfigFile(fmt.Sprintf("%s.name", securityTestName)),
+		Image:            dF.Caller.GetStringFromConfigFile(fmt.Sprintf("%s.image", securityTestName)),
+		Cmd:              dF.Caller.GetStringFromConfigFile(fmt.Sprintf("%s.cmd", securityTestName)),
+		Language:         dF.Caller.GetStringFromConfigFile(fmt.Sprintf("%s.language", securityTestName)),
+		Default:          dF.Caller.GetBoolFromConfigFile(fmt.Sprintf("%s.default", securityTestName)),
+		TimeOutInSeconds: dF.Caller.GetIntFromConfigFile(fmt.Sprintf("%s.timeOutInSeconds", securityTestName)),
 	}
 }
 
-func getMaxContainersAllowed() int {
-	maxContainersAllowed, err := strconv.Atoi(os.Getenv("HUSKYCI_DOCKERAPI_MAX_CONTAINERS_BEFORE_CLEANING"))
+func (dF DefaultConfig) GetMaxContainersAllowed() int {
+	maxContainersAllowed, err := dF.Caller.ConvertStrToInt(dF.Caller.GetEnvironmentVariable("HUSKYCI_DOCKERAPI_MAX_CONTAINERS_BEFORE_CLEANING"))
 	if err != nil {
 		return 50
 	}
