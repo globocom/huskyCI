96af9ad827fd99ecb777c8e03e066f58b040eb75
carlosljr <carlosjunior@id.uff.br>
2019-08-19 18:04:43 -0300

    [FEATURE] Define unit tests in context package.
    This garantees code coverage for the functions which belongs to context package.

---
+++ api/context/types.go
@@ -0,0 +1,50 @@
+package context
+
+import (
+	"github.com/spf13/viper"
+	"os"
+	"strconv"
+	"time"
+)
+
+type ExternalCalls struct{}
+
+func (eC *ExternalCalls) SetConfigFile(configName, configPath string) error {
+	viper.SetConfigFile(configName)
+	viper.AddConfigPath(configPath)
+	return viper.ReadInConfig()
+}
+
+func (eC *ExternalCalls) GetEnvironmentVariable(envName string) string {
+	return os.Getenv(envName)
+}
+
+func (eC *ExternalCalls) ConvertStrToInt(str string) (int, error) {
+	return strconv.Atoi(str)
+}
+
+func (eC *ExternalCalls) GetTimeDurationInSeconds(duration int) time.Duration {
+	return time.Duration(duration) * time.Second
+}
+
+func (eC *ExternalCalls) GetStringFromConfigFile(value string) string {
+	return viper.GetString(value)
+}
+
+func (eC *ExternalCalls) GetBoolFromConfigFile(value string) bool {
+	return viper.GetBool(value)
+}
+
+func (eC *ExternalCalls) GetIntFromConfigFile(value string) int {
+	return viper.GetInt(value)
+}
+
+type CallerInterface interface {
+	SetConfigFile(configName, configPath string) error
+	GetStringFromConfigFile(value string) string
+	GetBoolFromConfigFile(value string) bool
+	GetIntFromConfigFile(value string) int
+	GetEnvironmentVariable(envName string) string
+	ConvertStrToInt(str string) (int, error)
+	GetTimeDurationInSeconds(duration int) time.Duration
+}
