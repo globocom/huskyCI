4d8c8440bf7e3107c9bc782cd3c16e4952be0fb0
Rafael dos Santos <rafaveira3@gmail.com>
2019-08-27 17:42:24 -0300

    docs|FEAT: Add initial docusaurus files

--- deployments/scripts/create-certs.sh
+++ deployments/scripts/create-certs.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+
 # Copyright 2019 Globo.com authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
-
-#!/bin/bash
 #
 # This script will create certs so that huskyCI_Docker_API only accepts authenticated requests
 # from HuskyCI.
--- deployments/scripts/generate-env.sh
+++ deployments/scripts/generate-env.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 # Copyright 2019 Globo.com authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
-
-#!/bin/bash
+#
 #
 # This script will generate environment passwords to build huskyCI environment.
 #
