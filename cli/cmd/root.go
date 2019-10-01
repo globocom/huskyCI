e63246bc14cf805360e897d2e9479e225edc7fa6
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2019-10-01 13:35:04 -0300

    [FEAT] Add root command for huskyci CLI

---
+++ cli/cmd/root.go
@@ -0,0 +1,130 @@
+// Copyright © 2019 Globo.com
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "husky",
+	Short: "husky is a CLI to use huskyCI",
+	Long: `husky is a CLI to use huskyCI.
+	
+huskyCI is an open-source tool that performs security tests
+inside CI pipelines of multiple projects and centralizes all results 
+into a database for further analysis and metrics.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.huskyci.yaml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Printf("Client error reading home folder: %s (%s)\n", home, err.Error())
+			os.Exit(1)
+		}
+
+		// check if .huskyci folder exists and creates if it not exists
+		path := home + "/.huskyci"
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			err := os.Mkdir(path, 0750)
+			if err != nil {
+				fmt.Printf("Client error creating config folder: %s (%s)\n", path, err.Error())
+				os.Exit(1)
+			}
+			fmt.Printf("Client created config folder: %s\n", path)
+		}
+
+		// add path to viper config
+		viper.AddConfigPath(path)
+
+		// set config file name and type
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+
+		// test if config file exists and creates if it not exists
+		configFile := path + "/config.yaml"
+		err = viper.ReadInConfig()
+		if err != nil {
+			f, err := os.Create(configFile)
+			if err != nil {
+				fmt.Printf("Client error creating config file: %s (%s)\n", configFile, err.Error())
+				os.Exit(1)
+			}
+			err = f.Close()
+			if err != nil {
+				fmt.Printf("Client error closing config file: %s (%s)\n", configFile, err.Error())
+				os.Exit(1)
+			}
+			fmt.Printf("Client created new config file: %s\n", configFile)
+		}
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("Client error reading config file (%s)\n", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Using config file: %s\n\n", viper.ConfigFileUsed())
+}
