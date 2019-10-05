162868034078d053a2dcbe0f3c5e23f7419b81da
joserenatosilva <11424945+joserenatosilva@users.noreply.github.com>
2019-10-05 10:33:01 -0300

    Fix broken tests, client RID validation and client cert pool (#387)
    * client|FIX: get Root CAs from /etc/ssl/cert.pem
    
    * api|FIX: broken tests
    
    * client|FIX: RID validation, client logging, certs

--- api/util/api/api_suite_test.go
+++ api/util/api/api_suite_test.go
@@ -3,11 +3,31 @@ package util_test
 import (
 	"testing"
 
+	apiContext "github.com/globocom/huskyCI/api/context"
+	"github.com/globocom/huskyCI/api/log"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestApi(t *testing.T) {
 	RegisterFailHandler(Fail)
+	TestInitLog(t)
 	RunSpecs(t, "Api Suite")
+}
+
+func TestInitLog(t *testing.T) {
+	apiContext.APIConfiguration = &apiContext.APIConfig{
+		GraylogConfig: &apiContext.GraylogConfig{
+			DevelopmentEnv: true,
+			AppName:        "log_test",
+			Tag:            "log_test",
+		},
+	}
+
+	log.InitLog()
+
+	if log.Logger == nil {
+		t.Error("expected logger to be initialized, but it wasn't")
+		return
+	}
 }
