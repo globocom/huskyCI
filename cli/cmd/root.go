9ae1c2abdec96e4568e0bcac5c1094f05308fc12
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2019-10-08 16:25:44 -0300

    [FIX] Remove comments about persistent flags

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -64,9 +64,6 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.huskyci/config.yaml)")
 }
 
