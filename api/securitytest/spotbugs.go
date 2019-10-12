c8ab54e91d8d360159ec59690e972967f76e7ef7
abzcoding <abzcoding@gmail.com>
2019-10-12 13:28:03 +0330

    parse XML output from spotbugs

--- api/securitytest/spotbugs.go
+++ api/securitytest/spotbugs.go
@@ -5,33 +5,62 @@
 package securitytest
 
 import (
-	"encoding/json"
+	"encoding/xml"
 
 	"github.com/globocom/huskyCI/api/log"
 	"github.com/globocom/huskyCI/api/types"
 )
 
 // SpotBugsOutput is the struct that holds all data from SpotBugs output.
-type SpotBugsOutput []SpotBugsIssue
+type SpotBugsOutput struct {
+	XMLName       xml.Name        `xml:"BugCollection"`
+	Project       Project         `xml:"Project"`
+	SpotBugsIssue []SpotBugsIssue `xml:"BugInstance"`
+	Errors        Error           `xml:"Errors"`
+}
+
+// Project is the struct that holds data about project
+type Project struct {
+	XMLName xml.Name `xml:"Project"`
+	Name    string   `xml:"projectName,attr"`
+	Jar     string   `xml:"Jar"`
+	Plugin  string   `xml:"Plugin"`
+}
 
 // SpotBugsIssue is the struct that holds all issues from SpotBugs output.
 type SpotBugsIssue struct {
-	Type         string `json:"type"`
-	Priority     string `json:"priority"`
-	Rank         string `json:"rank"`
-	Abbreviation string `json:"abbrev"`
-	Category     string `json:"category"`
-	// Files []SpotBugFile
+	XMLName      xml.Name     `xml:"BugInstance"`
+	Type         string       `xml:"type,attr"`
+	Priority     string       `xml:"priority,attr"`
+	Rank         string       `xml:"rank,attr"`
+	Abbreviation string       `xml:"abbrev,attr"`
+	Category     string       `xml:"category,attr"`
+	SourceLine   []SourceLine `xml:"SourceLine"`
 }
 
-// type SpotBugFile struct{
+// Error is the struct that holds errors that happened in analysis
+type Error struct {
+	XMLName        xml.Name `xml:"Errors"`
+	Errors         string   `xml:"errors,attr"`
+	MissingClasses string   `xml:"missingClasses,attr"`
+}
 
-// }
+// SourceLine is the struct that holds details about issue location
+type SourceLine struct {
+	XMLName       xml.Name `xml:"SourceLine"`
+	ClassName     string   `xml:"classname,attr"`
+	Start         string   `xml:"start,attr"`
+	End           string   `xml:"end,attr"`
+	StartByteCode string   `xml:"startBytecode,attr"`
+	EndByteCode   string   `xml:"endBytecode,attr"`
+	SourceFile    string   `xml:"sourcefile,attr"`
+	SourcePath    string   `xml:"sourcepath,attr"`
+}
 
 func analyzeSpotBugs(spotbugsScan *SecTestScanInfo) error {
 
-	goSecOutput := SpotBugsOutput{}
-	spotbugsScan.FinalOutput = goSecOutput
+	spotBugsOutput := SpotBugsOutput{}
+	spotbugsScan.FinalOutput = spotBugsOutput
 
 	// nil cOutput states that no Issues were found.
 	if spotbugsScan.Container.COutput == "" {
@@ -40,43 +69,61 @@ func analyzeSpotBugs(spotbugsScan *SecTestScanInfo) error {
 	}
 
 	// Unmarshall rawOutput into finalOutput, that is a SpotBugsOutput struct.
-	if err := json.Unmarshal([]byte(spotbugsScan.Container.COutput), &goSecOutput); err != nil {
+	spotBugsOutput, err := parseXMLtoJSON([]byte(spotbugsScan.Container.COutput))
+	if err != nil {
 		log.Error("analyzeSpotBugs", "SPOTBUGS", 1039, spotbugsScan.Container.COutput, err)
 		spotbugsScan.ErrorFound = err
 		spotbugsScan.prepareContainerAfterScan()
 		return err
 	}
-	spotbugsScan.FinalOutput = goSecOutput
+
+	spotbugsScan.FinalOutput = spotBugsOutput
 
 	// check results and prepare all vulnerabilities found
 	spotbugsScan.prepareSpotBugsVulns()
 	spotbugsScan.prepareContainerAfterScan()
 	return nil
 }
+func parseXMLtoJSON(byteValue []byte) (SpotBugsOutput, error) {
+	var bugs SpotBugsOutput
+	if err := xml.Unmarshal(byteValue, &bugs); err != nil {
+		return bugs, err
+	}
+	// if numOfErrors, err := strconv.Atoi(bugs.Errors.Errors); err == nil {
+	// 	if numOfErrors > 0 {
+	// 		fmt.Println("Errors happened")
+	// 	}
+	// }
+	// if numOfMissingClasses, err := strconv.Atoi(bugs.Errors.MissingClasses); err == nil {
+	// 	if numOfMissingClasses > 0 {
+	// 		fmt.Println("missing classes")
+	// 	}
+	// }
+	return bugs, nil
+}
 
 func (spotbugsScan *SecTestScanInfo) prepareSpotBugsVulns() {
+	var spotbugsOutput SpotBugsOutput
 
 	huskyCIspotbugsResults := types.HuskyCISecurityTestOutput{}
-	spotbugsOutput := spotbugsScan.FinalOutput.(SpotBugsOutput)
+	spotbugsOutput = spotbugsScan.FinalOutput.(SpotBugsOutput)
 
-	for _, issue := range spotbugsOutput {
-		spotbugsVuln := types.HuskyCIVulnerability{}
-		spotbugsVuln.Language = "Java"
-		spotbugsVuln.SecurityTool = "SpotBugs"
-		spotbugsVuln.Severity = issue.Priority
-		// spotbugsVuln.Confidence = issue.Confidence
-		// spotbugsVuln.Details = issue.Details
-		// spotbugsVuln.File = issue.File
-		// spotbugsVuln.Line = issue.Line
-		// spotbugsVuln.Code = issue.Code
-
-		switch spotbugsVuln.Severity {
-		case "LOW":
-			huskyCIspotbugsResults.LowVulns = append(huskyCIspotbugsResults.LowVulns, spotbugsVuln)
-		case "MEDIUM":
-			huskyCIspotbugsResults.MediumVulns = append(huskyCIspotbugsResults.MediumVulns, spotbugsVuln)
-		case "HIGH":
-			huskyCIspotbugsResults.HighVulns = append(huskyCIspotbugsResults.HighVulns, spotbugsVuln)
+	for i := 0; i < len(spotbugsOutput.SpotBugsIssue); i++ {
+		for j := 0; j < len(spotbugsOutput.SpotBugsIssue[i].SourceLine); j++ {
+			spotbugsVuln := types.HuskyCIVulnerability{}
+			spotbugsVuln.Language = "Java"
+			spotbugsVuln.SecurityTool = "SpotBugs"
+			spotbugsVuln.Details = spotbugsOutput.SpotBugsIssue[i].Type
+			spotbugsVuln.File = spotbugsOutput.SpotBugsIssue[i].SourceLine[j].SourcePath
+			spotbugsVuln.Line = spotbugsOutput.SpotBugsIssue[i].SourceLine[j].Start
+			switch spotbugsOutput.SpotBugsIssue[i].Priority {
+			case "1":
+				huskyCIspotbugsResults.HighVulns = append(huskyCIspotbugsResults.HighVulns, spotbugsVuln)
+			case "2":
+				huskyCIspotbugsResults.MediumVulns = append(huskyCIspotbugsResults.MediumVulns, spotbugsVuln)
+			default:
+				huskyCIspotbugsResults.LowVulns = append(huskyCIspotbugsResults.LowVulns, spotbugsVuln)
+			}
 		}
 	}
 
