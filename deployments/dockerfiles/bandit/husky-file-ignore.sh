ad39ec5d8e32f1cc7a92da3193b3b4353c4df193
Daniel Carlier[user] <daniel.carlier@hotmail.com>
2020-02-27 17:20:16 -0300

    dep|FEAT: Break script into functions

--- deployments/dockerfiles/bandit/husky-file-ignore.sh
+++ deployments/dockerfiles/bandit/husky-file-ignore.sh
@@ -8,15 +8,68 @@
 #
 
 huskyCIFile=".huskyci"
+codePath="/code/"
+
+isHuskyIgnore(){
+    line=$1
+
+    if echo "$line" | grep -q "huskyCI-Ignore"; then
+        return 0
+    fi
+
+    return 1
+}
+
+isCommented(){
+    line=$1
+    commentRegexp='^[[:space:]]*#'
+
+    if echo "$line" | grep -Eq "$commentRegexp"; then
+        return 0
+    fi
+
+    return 1
+}
+
+isEmpty(){
+    line=$1
+
+    if [ ! "$line" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
+leavesCodePath(){
+    line=$1
+
+    if echo "$line" | grep -qF "../"; then
+        return 0
+    fi
+
+    return 1
+}
+
+wouldRemoveCurrentWorkdir() {
+    line=$1
+
+    if echo "$codePath$line" | grep -qF "//"; then
+        return 0
+    fi
+
+    return 1
+}
 
 if [ -f "$huskyCIFile" ]; then
+
     while IFS= read -r line; do
-	commentRegexp='^[[:space:]]*#'
-        if echo "$line" | grep -q "huskyCI-Ignore" || echo "$line" | grep -Eq "$commentRegexp" || [ ! "$line" ]; then
+	    
+        if isHuskyIgnore $line || isCommented $line || isEmpty $line || leavesCodePath $line || wouldRemoveCurrentWorkdir $line; then
             continue
         fi
 
-        rm -rf $line
+        rm -rf "$codePath$line"
 
     done < "$huskyCIFile"
 fi
