7a7332e03ee6a7257756080ccf67f2c1ac3c82ef
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-05-19 03:22:15 +0200

    Feature: Add struct TFSec

---
+++ api/securitytest/tfsec.go
@@ -0,0 +1,88 @@
+// Copyright 2019 Globo.com authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package securitytest
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/globocom/huskyCI/api/log"
+	"github.com/globocom/huskyCI/api/types"
+)
+
+// TFSecOutput is the struct that holds all data from TFSec output.
+type TFSecOutput struct {
+	Warnings json.RawMessage `json:"warnings"`
+	Results  []TFSecResult   `json:"results"`
+}
+
+// Result is the struct that holds detailed information of issues from TFSec output.
+type TFSecResult struct {
+	RuleID      string   `json:"rule_id"`
+	Link        string   `json:"link"`
+	Location    Location `json:"location"`
+	Description string   `json:"description"`
+	Severity    string   `json:"severity"`
+}
+
+type Location struct {
+	Filename  string `json:"filename"`
+	StartLine string `json:"start_line"`
+	EndLine   string `json:"end_line"`
+}
+
+func analyzeTFSec(tfsecScan *SecTestScanInfo) error {
+
+	tfsecOutput := TFSecOutput{}
+
+	// Unmarshall rawOutput into finalOutput, that is a TFSec struct.
+	if err := json.Unmarshal([]byte(tfsecScan.Container.COutput), &tfsecOutput); err != nil {
+		log.Error("analyzeTFSec", "TFSEC", 1040, tfsecScan.Container.COutput, err)
+		tfsecScan.ErrorFound = err
+		return err
+	}
+	tfsecScan.FinalOutput = tfsecOutput
+
+	// an empty Results slice states that no Issues were found.
+	if tfsecOutput.Results == nil {
+		tfsecScan.prepareContainerAfterScan()
+		return nil
+	}
+
+	// check results and prepare all vulnerabilities found
+	tfsecScan.prepareTFSecVulns()
+	tfsecScan.prepareContainerAfterScan()
+	return nil
+}
+
+func (tfsecScan *SecTestScanInfo) prepareTFSecVulns() {
+
+	huskyCItfsecResults := types.HuskyCISecurityTestOutput{}
+	tfsecOutput := tfsecScan.FinalOutput.(TFSecOutput)
+
+	for _, result := range tfsecOutput.Results {
+		tfsecVuln := types.HuskyCIVulnerability{}
+		tfsecVuln.Language = "HCL"
+		tfsecVuln.SecurityTool = "TFSec"
+		tfsecVuln.Severity = result.Severity
+		tfsecVuln.Details = result.RuleID + " @ [" + result.Description + "]"
+		startLine := result.Location.StartLine
+		endLine := result.Location.EndLine
+		tfsecVuln.Line = startLine
+		tfsecVuln.Code = fmt.Sprintf("Code beetween Line %s and Line %s.", startLine, endLine)
+		tfsecVuln.File = result.Location.Filename
+
+		switch tfsecVuln.Severity {
+		case "INFO":
+			huskyCItfsecResults.LowVulns = append(huskyCItfsecResults.LowVulns, tfsecVuln)
+		case "WARNING":
+			huskyCItfsecResults.MediumVulns = append(huskyCItfsecResults.MediumVulns, tfsecVuln)
+		case "ERROR":
+			huskyCItfsecResults.HighVulns = append(huskyCItfsecResults.HighVulns, tfsecVuln)
+		}
+	}
+
+	tfsecScan.Vulnerabilities = huskyCItfsecResults
+}
