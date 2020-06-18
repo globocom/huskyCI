892ca24a7da98a30873ef0add348f88b48ece485
stormhax <vgood@protonmail.com>
2020-06-18 16:53:44 +0200

    feat: Align bandit with defectdojo plugin.
    To easily integrate HuskyCi with DefectDojo -proper title field is required: https://github.com/DefectDojo/django-DefectDojo/blob/master/dojo/tools/bandit/parser.py#L30

--- api/securitytest/bandit.go
+++ api/securitytest/bandit.go
@@ -69,6 +69,7 @@ func (banditScan *SecTestScanInfo) prepareBanditVulns() {
 		}
 		banditVuln.Severity = issue.IssueSeverity
 		banditVuln.Confidence = issue.IssueConfidence
+		banditVuln.Title = "Test Name: " + issue.TestName + " Test ID: " + issue.TestID
 		banditVuln.Details = issue.IssueText
 		banditVuln.File = issue.Filename
 		banditVuln.Line = strconv.Itoa(issue.LineNumber)
