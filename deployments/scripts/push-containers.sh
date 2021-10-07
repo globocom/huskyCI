af07292b72e81e88b67218f49c84893d9201d33f
Pablo Aguilar <pablo.aguilar@outlook.com.br>
2021-10-07 03:13:38 -0300

    Creates `infer` deployment files

--- deployments/scripts/push-containers.sh
+++ deployments/scripts/push-containers.sh
@@ -18,6 +18,7 @@ safetyVersion=$(docker run --rm huskyci/safety:latest safety --version | awk -F 
 gitleaksVersion=$(docker run --rm huskyci/gitleaks:latest gitleaks --version)
 spotbugsVersion=$(docker run --rm huskyci/spotbugs:latest cat /opt/spotbugs/version)
 tfsecVersion=$(docker run --rm huskyci/tfsec:latest ./tfsec -v)
+inferVersion=$(docker run --rm huskyci/infer:latest infer --version | grep Infer | awk -F " " '{print $3}')
 
 docker tag "huskyci/bandit:latest" "huskyci/bandit:$banditVersion"
 docker tag "huskyci/brakeman:latest" "huskyci/brakeman:$brakemanVersion"
@@ -30,6 +31,7 @@ docker tag "huskyci/safety:latest" "huskyci/safety:$safetyVersion"
 docker tag "huskyci/gitleaks:latest" "huskyci/gitleaks:$gitleaksVersion"
 docker tag "huskyci/spotbugs:latest" "huskyci/spotbugs:$spotbugsVersion"
 docker tag "huskyci/tfsec:latest" "huskyci/tfsec:$tfsecVersion"
+docker tag "huskyci/infer:latest" "huskyci/infer:$inferVersion"
 
 docker push "huskyci/bandit:latest" && docker push "huskyci/bandit:$banditVersion"
 docker push "huskyci/brakeman:latest" && docker push "huskyci/brakeman:$brakemanVersion"
@@ -42,3 +44,4 @@ docker push "huskyci/safety:latest" && docker push "huskyci/safety:$safetyVersio
 docker push "huskyci/gitleaks:latest" && docker push "huskyci/gitleaks:$gitleaksVersion"
 docker push "huskyci/spotbugs:latest" && docker push "huskyci/spotbugs:$spotbugsVersion"
 docker push "huskyci/tfsec:latest" && docker push "huskyci/tfsec:$tfsecVersion"
+docker push "huskyci/infer:latest" && docker push "huskyci/infer:$inferVersion"
