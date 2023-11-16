edf5b62e97526a0de791efc7e5d8d9ba369e175a
Michel Aquino <michelaquino@mailbox.org>
2023-11-16 13:53:18 -0300

    Add find all integration tests

--- api/db/huskydb_integration_test.go
+++ api/db/huskydb_integration_test.go
@@ -50,7 +50,7 @@ var _ = AfterSuite(func() {
 	Expect(err).To(BeNil())
 })
 
-var _ = Describe("FindOneDBRepository", func() {
+var _ = Describe("DBRepository", func() {
 	Context("When try to find one", func() {
 		It("Should return mgo.ErrNotFound and empty types.Repository{} when not found", func() {
 			mapParams := map[string]interface{}{"repositoryURL": "not found URL"}
@@ -76,9 +76,48 @@ var _ = Describe("FindOneDBRepository", func() {
 			Expect(repository).To(Equal(repositoryToInsert))
 		})
 	})
+	Context("When try to find all", func() {
+		It("Should return no error and empty repository array when not found", func() {
+			mapParams := map[string]interface{}{"repositoryURL": "not found URL"}
+
+			repository, err := huskydbMongoRequestsTest.FindAllDBRepository(mapParams)
+			Expect(err).To(BeNil())
+
+			expectedResult := []types.Repository{}
+			Expect(repository).To(Equal(expectedResult))
+		})
+		It("Should return no error and types.Repository{} correctly", func() {
+			repositoryToInsert1 := types.Repository{
+				URL: "http://github.com/findallrepository",
+			}
+
+			errInsert := huskydbMongoRequestsTest.InsertDBRepository(repositoryToInsert1)
+			Expect(errInsert).To(BeNil())
+
+			repositoryToInsert2 := types.Repository{
+				URL: "http://github.com/findallrepository",
+			}
+
+			errInsert = huskydbMongoRequestsTest.InsertDBRepository(repositoryToInsert2)
+			Expect(errInsert).To(BeNil())
+
+			mapParams := map[string]interface{}{
+				"repositoryURL": "http://github.com/findallrepository",
+			}
+
+			expectedResult := []types.Repository{
+				repositoryToInsert1,
+				repositoryToInsert2,
+			}
+
+			result, errGet := huskydbMongoRequestsTest.FindAllDBRepository(mapParams)
+			Expect(errGet).To(BeNil())
+			Expect(result).To(Equal(expectedResult))
+		})
+	})
 })
 
-var _ = Describe("FindOneDBSecurityTest", func() {
+var _ = Describe("DBSecurityTest", func() {
 	Context("When try to find one", func() {
 		It("Should return mgo.ErrNotFound and empty  types.SecurityTest{} when not found", func() {
 			mapParams := map[string]interface{}{"name": "security_test_name"}
@@ -95,7 +134,7 @@ var _ = Describe("FindOneDBSecurityTest", func() {
 				Image:            "some image",
 				Cmd:              "some cmd",
 				Language:         "some language",
-				Type:             "some tipe",
+				Type:             "some type",
 				Default:          false,
 				TimeOutInSeconds: 10,
 			}
@@ -110,9 +149,57 @@ var _ = Describe("FindOneDBSecurityTest", func() {
 			Expect(repository).To(Equal(securityTestToInsert))
 		})
 	})
+	Context("When try to find all", func() {
+		It("Should return no error and empty types.SecurityTest{} array when not found", func() {
+			mapParams := map[string]interface{}{"type": "type_find_all"}
+
+			repository, err := huskydbMongoRequestsTest.FindAllDBSecurityTest(mapParams)
+			Expect(err).To(BeNil())
+
+			expectedResult := []types.SecurityTest{}
+			Expect(repository).To(Equal(expectedResult))
+		})
+		It("Should return no error and  types.SecurityTest{} correctly", func() {
+			securityTestToInsert1 := types.SecurityTest{
+				Name:             "security_test_name_1",
+				Image:            "some image",
+				Cmd:              "some cmd",
+				Language:         "some language",
+				Type:             "type_find_all",
+				Default:          false,
+				TimeOutInSeconds: 10,
+			}
+
+			errInsert := huskydbMongoRequestsTest.InsertDBSecurityTest(securityTestToInsert1)
+			Expect(errInsert).To(BeNil())
+
+			securityTestToInsert2 := types.SecurityTest{
+				Name:             "security_test_name_2",
+				Image:            "some image",
+				Cmd:              "some cmd",
+				Language:         "some language",
+				Type:             "type_find_all",
+				Default:          false,
+				TimeOutInSeconds: 10,
+			}
+
+			errInsert = huskydbMongoRequestsTest.InsertDBSecurityTest(securityTestToInsert2)
+			Expect(errInsert).To(BeNil())
+
+			expectResult := []types.SecurityTest{
+				securityTestToInsert1,
+				securityTestToInsert2,
+			}
+
+			mapParams := map[string]interface{}{"type": "type_find_all"}
+			repository, errGet := huskydbMongoRequestsTest.FindAllDBSecurityTest(mapParams)
+			Expect(errGet).To(BeNil())
+			Expect(repository).To(Equal(expectResult))
+		})
+	})
 })
 
-var _ = Describe("FindOneDBAnalysis", func() {
+var _ = Describe("DBAnalysis", func() {
 	Context("When try to find one", func() {
 		It("Should return mgo.ErrNotFound and empty  types.Analysis{} when not found", func() {
 			mapParams := map[string]interface{}{"RID": "test-id"}
@@ -143,9 +230,55 @@ var _ = Describe("FindOneDBAnalysis", func() {
 			Expect(analysis).To(Equal(analysisToInsert))
 		})
 	})
+	Context("When try to find all", func() {
+		It("Should return no error and empty types.Analysis{} array when not found", func() {
+			mapParams := map[string]interface{}{"status": "status-find-all-not-found"}
+
+			analysis, err := huskydbMongoRequestsTest.FindAllDBAnalysis(mapParams)
+			Expect(err).To(BeNil())
+
+			expectedResult := []types.Analysis{}
+			Expect(analysis).To(Equal(expectedResult))
+		})
+		It("Should return no error and []types.Analysis{} correctly", func() {
+			analysisToInsert1 := types.Analysis{
+				RID:        "test-id-1",
+				URL:        "some url",
+				Branch:     "some branch",
+				Status:     "status-find-all",
+				Result:     "some result",
+				Containers: []types.Container{},
+			}
+
+			errInsert := huskydbMongoRequestsTest.InsertDBAnalysis(analysisToInsert1)
+			Expect(errInsert).To(BeNil())
+
+			analysisToInsert2 := types.Analysis{
+				RID:        "test-id-2",
+				URL:        "some url",
+				Branch:     "some branch",
+				Status:     "status-find-all",
+				Result:     "some result",
+				Containers: []types.Container{},
+			}
+
+			errInsert = huskydbMongoRequestsTest.InsertDBAnalysis(analysisToInsert2)
+			Expect(errInsert).To(BeNil())
+
+			expectedResult := []types.Analysis{
+				analysisToInsert1,
+				analysisToInsert2,
+			}
+
+			mapParams := map[string]interface{}{"status": "status-find-all"}
+			analysis, errGet := huskydbMongoRequestsTest.FindAllDBAnalysis(mapParams)
+			Expect(errGet).To(BeNil())
+			Expect(analysis).To(Equal(expectedResult))
+		})
+	})
 })
 
-var _ = Describe("FindOneDBUser", func() {
+var _ = Describe("DBUser", func() {
 	Context("When try to find one", func() {
 		It("Should return mgo.ErrNotFound and empty  types.User{} when not found", func() {
 			mapParams := map[string]interface{}{"username": "some user name"}
@@ -178,7 +311,7 @@ var _ = Describe("FindOneDBUser", func() {
 	})
 })
 
-var _ = Describe("FindOneDBAccessToken", func() {
+var _ = Describe("DBAccessToken", func() {
 	Context("When try to find one", func() {
 		It("Should return mgo.ErrNotFound and empty  types.DBToken{} when not found", func() {
 			mapParams := map[string]interface{}{"uuid": "some uuid"}
